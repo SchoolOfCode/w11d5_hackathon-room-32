b355c007a8875fa305aba7b924592e12070d1db9
Laurence279 <lnunn279@gmail.com>
2022-02-04 16:15:27 +0000

    Add timer

--- card-app/components/Timer/index.js
+++ card-app/components/Timer/index.js
@@ -1,12 +1,19 @@
 import React from "react";
+import {useRef, useState, useEffect} from "react";
 import css from "./Timer.module.css";
 
 function Timer() {
-  return (
+  const intervalRef = useRef(null);
+  const [num, setNum] = useState(10);  const decreaseNum = () => setNum((prev) => prev - 1);  useEffect(() => {
+    intervalRef.current = setInterval(decreaseNum, 1000);    return () => clearInterval(intervalRef.current);
+  }, []);  useEffect(() => {
+    if (num === 0) {
+      setNum(0);
+    }
+  });  return (
     <div className={css.timer}>
-      <h1 className={css.time}>00:10</h1>
+      <h1 className={css.time}>{num <= 0 ? "0" : num}</h1>
     </div>
   );
 }
-
 export default Timer;
